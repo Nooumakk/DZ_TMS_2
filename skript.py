d81e46e62baf704f317ccb3c1485686ef9f95382
Daniel <cskteam4@gmail.com>
2022-09-08 12:27:16 +0300

    Оптимизация кода

--- skript.py
+++ skript.py
@@ -8,16 +8,12 @@ def func():
         for line in f.readlines():
             lines.append(line.rstrip().split(',')) # Добавление всех строк файла без переносов строки.
 
-        keys = lines[0]
         value = []
-
         for i in range(1, len(lines)): # Цикл, выполняемый столько раз, сколько есть элементов в списке "lines" -1.
-            Dictionary = zip(keys, lines[i]) # Объединение 2 объектов в один кортеж.
-            value.append(dict(Dictionary)) # Добавление кортежа в список, с последующим превращением его в словарь.
+            value.append(dict(zip(lines[0], lines[i]))) # Добавление кортежа в список, с последующим превращением его в словарь.
 
         with open("data.file.json", "w") as write_file: # Создание нового файла формата .json, открытие его в режиме изменения.
             json.dump(value, write_file) # Сохранение полученной информации в данном файле.
-
         return value
 
 
