bb093cf9a10978f360677a6d2dd2a18cf3869379
Daniel <cskteam4@gmail.com>
2022-09-09 13:33:54 +0300

    Оптимизация кода, исправление представленной информации в json.

--- skript.py
+++ skript.py
@@ -5,20 +5,23 @@ def func():
     with open('Table.csv', 'r') as f: # Открытие файла в режиме чтения.
 
         lines = []
-        for line in f.readlines():
-            lines.append(line.rstrip().split(',')) # Добавление всех строк файла без переносов строки.
-
         value = []
-        for i in range(1, len(lines)): # Цикл, выполняемый столько раз, сколько есть элементов в списке "lines" -1.
-            value.append(dict(zip(lines[0], lines[i]))) # Добавление кортежа в список, с последующим превращением его в словарь.
+        
+        for line in f.readlines(): # Перебор всех строк в файле csv
+            lines.append(line.rstrip().split(',')) # Добавление всех строк файла без переносов строки.
+            if len(lines) <= 1: # Ограничение на выпонение функции зип, пока в списке не будет 2 элемента
+                continue
+            map(value.append(dict(zip(lines[0], lines[1]))), lines) # Объединение заглавного и второстепенного списка в словарь, с сохранением в список.
+            del lines[1] # Удаление второстепенного списка.
 
+            
         with open("data.file.json", "w") as write_file: # Создание нового файла формата .json, открытие его в режиме изменения.
-            json.dump(value, write_file) # Сохранение полученной информации в данном файле.
-        return value
+            json.dump(value, write_file, indent=1) # Сохранение полученной информации в данном файле.
+    return value
 
 
 def main(): 
-    func()
+    print(func())
 
 
 if __name__ == "__main__":
